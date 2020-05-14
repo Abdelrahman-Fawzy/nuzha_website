da56d4e86b43e1f30550783126a0b79400d77734
ElafEssam <elafmuhammadessam@gmail.com>
2020-05-14 12:28:35 +0200

    recently add: tour Guid page

---
+++ dist/js/form.js
@@ -0,0 +1,13 @@
+var element = document.getElementsByClassName('chcolor');
+var btn = document.getElementsByClassName('btn-up');
+var image = document.getElementsByTagName('img');
+btn[0].style.backgroundColor = '#FFF';
+for ($i=0;$i<5;$i++) {
+element[$i].style.color = '#FFF';
+}
+image[0].src='../images/NavBar/logo-light.svg';
+image[1].src='../images/NavBar/1white.svg';
+image[2].src='../images/NavBar/2white.svg';
+image[3].src='../images/NavBar/3white.svg';
+image[4].src='../images/NavBar/4white.svg';
+image[5].src='../images/NavBar/5white.svg';
